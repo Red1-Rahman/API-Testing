9986a2c8542bfe5dbb8f14446b2dff829a6ee2d7
Redwan Rahman <69118850+Red1-Rahman@users.noreply.github.com>
2026-02-12 22:09:33 +0600

    Add basic sanity check script for API chat

---
+++ templates/basic-sanity.sh
@@ -0,0 +1,7 @@
+curl -X POST http://localhost:3001/api/chat \
+  -H "Content-Type: application/json" \
+  -d '{
+    "messages": [
+      {"role": "user", "content": "Hello, who are you?"}
+    ]
+  }'
