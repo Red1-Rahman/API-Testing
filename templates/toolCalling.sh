5078552a51946bcdb5ad8b23072f3d8f819e5fac
Redwan Rahman <69118850+Red1-Rahman@users.noreply.github.com>
2026-02-12 22:12:06 +0600

    Create toolCalling.sh for weather API calls
    Add toolCalling.sh to make API requests for weather

---
+++ templates/toolCalling.sh
@@ -0,0 +1,19 @@
+curl -X POST http://localhost:3001/api/chat \
+  -H "Content-Type: application/json" \
+  -d '{
+    "messages": [{"role": "user", "content": "What is the weather in Dhaka?"}],
+    "tools": [
+      {
+        "type": "function",
+        "function": {
+          "name": "get_weather",
+          "parameters": {
+            "type": "object",
+            "properties": {
+              "city": {"type": "string"}
+            }
+          }
+        }
+      }
+    ]
+  }'
